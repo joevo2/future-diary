624f3be7380de010fb0777cd3c7490f7e1323c9f
Joel Yek SE <joevo2@gmail.com>
2013-07-31 07:07:16 +0000

    Initial index.html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
+var content = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(content.toString("utf-8"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
