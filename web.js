2b922bd309a1d499dcc41699ae6fd2f30da44242
Joel Yek <joevo2@gmail.com>
2013-08-16 19:32:21 +0800

    experimneting with web.js to serve about.html

--- web.js
+++ web.js
@@ -3,9 +3,15 @@ var app = express();
 app.use(express.logger());
 var fs = require('fs');
 var content = fs.readFileSync('index.html');
+var content1 = fs.readFileSync('about.html');
 
 app.get('/', function(request, response) {
   response.send(content.toString("utf-8"));
+});
+
+
+app.get('/about.html', function(request, response) {
+  response.send(content1.toString("utf-8"));
 });
 
 var port = process.env.PORT || 8080;
